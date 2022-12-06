2477287732d5357dbfbf5136451cc55f0b010238
Deep Sethi <dsethi@infostatsystems.com>
2022-12-06 14:50:12 -0800

    Addressed error printing.

--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
 def test_outcome(test_name: str, value: any, expectation: any) -> None:
     if (value != expectation):
         print("FAIL: " + test_name)
-        print("Expected: " + expectation)
-        print("Received: " + value)
+        print("Expected: " + str(expectation))
+        print("Received: " + str(value))
         exit(1)
     else:
         print(test_name.upper() + " TESTS PASS!")
